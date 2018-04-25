916aacbe4e24c2702ef6f5931f57f947e4a5b53f
Jean-Baptiste ROUBELAT <jbroube@hotmail.com>
2018-04-25 21:01:27 +0200

    Entites

---
+++ Spacelib/Spacelib-ejb/src/main/java/entities/Mecanicien.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jb
+ */
+@Entity
+public class Mecanicien extends Utilisateur implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String toString() {
+        return "entities.Mechanician[ id=" + this.getId() + " ]";
+    }
+    
+}
