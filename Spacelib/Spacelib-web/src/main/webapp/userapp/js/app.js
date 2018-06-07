b0a48c2558e18aebd8f6bf135fd528f90f1616b9
Jean-Baptiste ROUBELAT <jbroube@hotmail.com>
2018-06-07 12:37:18 +0200

    MàJ Jquery & Materialize + Setup Réserver

--- Spacelib/Spacelib-web/src/main/webapp/userapp/js/app.js
+++ Spacelib/Spacelib-web/src/main/webapp/userapp/js/app.js
@@ -3,6 +3,12 @@
 
     $('.sidenav').sidenav();
     $('select').formSelect();
+    $('.datepicker').datepicker({
+      autoClose : true,
+      defaultDate : new Date(),
+      minDate : new Date(),
+      format : 'dd/mm/yy'
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
